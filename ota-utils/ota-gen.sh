0a6d5bcf62ff89d52b35eece618a2168d59c7d56
Ben Clouser <ben.clouser@toradex.com>
2019-05-01 13:52:05 -0700

    Add ca.key to gateway-tls secret

--- ota-utils/ota-gen.sh
+++ ota-utils/ota-gen.sh
@@ -23,54 +23,6 @@ KUBE_AUTH=Authorization:"Bearer ${KUBE_API_TOKEN}"
 echo "KUBE_API_URL=${KUBE_API_URL}"
 echo "KUBE_AUTH=${KUBE_AUTH}"
 
-# TODO. Determine if we are getting rid of this or not
-new_client() {
-  export DEVICE_UUID=${DEVICE_UUID:-$(uuidgen | tr "[:upper:]" "[:lower:]")}
-  local device_id=${DEVICE_ID:-${DEVICE_UUID}}
-  local device_dir="${DEVICES_DIR}/${DEVICE_UUID}"
-  local api="${KUBE_API_URL}/${NAMESPACE}/services"
-  local device_registry="${api}/device-registry/proxy"
-  
-  # Append 8 characters to device_id to make it unique
-  device_id=${device_id}-"$(uuidgen | tr "[:upper:]" "[:lower:]" | fold -w 8 | head -n 1)"
-  
-  mkdir -p "${device_dir}"
-  echo ""
-  echo "=== New Device Creation ==="
-  echo "UUID for device = ${DEVICE_UUID}"
-  echo "device_id = ${device_id}"
-
-  # This is a tag for including a chunk of code in the docs. Don't remove. tag::genclientkeys[]
-  openssl ecparam -genkey -name prime256v1 | openssl ec -out "${device_dir}/pkey.ec.pem"
-  openssl pkcs8 -topk8 -nocrypt -in "${device_dir}/pkey.ec.pem" -out "${device_dir}/pkey.pem"
-  openssl req -new -config "${CWD}/certs/client.cnf" -key "${device_dir}/pkey.pem" -out "${device_dir}/${device_id}.csr"
-  openssl x509 -req -days 365 -extfile "${CWD}/certs/client.ext" -in "${device_dir}/${device_id}.csr" \
-    -CAkey "${DEVICES_DIR}/ca.key" -CA "${DEVICES_DIR}/ca.crt" -CAcreateserial -out "${device_dir}/client.pem"
-  cat "${device_dir}/client.pem" "${DEVICES_DIR}/ca.crt" > "${device_dir}/${device_id}.chain.pem"
-  ln -s "${SERVER_DIR}/server_ca.pem" "${device_dir}/ca.pem" || true
-  openssl x509 -in "${device_dir}/client.pem" -text -noout
-  # end::genclientkeys[]
-
-  echo "KUBE_AUTH: ${KUBE_AUTH}"
-  RESP_UUID=$(http --ignore-stdin --verify=no PUT "${device_registry}/api/v1/devices" credentials=@"${device_dir}/client.pem" \
-    deviceUuid="${DEVICE_UUID}" deviceId="${device_id}" deviceName="${device_id}" deviceType=Other "${KUBE_AUTH}")
-
-  echo "The Device Registry responded with a UUID OF: ${RESP_UUID}"
-  [[ ${SKIP_CLIENT} == true ]] && return 0
-
-  echo "ERROR TODO: Get rid of kubectl call here for new_client"
-  exit -1
-  local gateway=${GATEWAY_ADDR:-$(${KUBECTL} get nodes --output jsonpath \
-    --template='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')}
-  local addr=${DEVICE_ADDR:-localhost}
-  local port=${DEVICE_PORT:-2222}
-  local options="-o StrictHostKeyChecking=no"
-
-  ssh ${options} "root@${addr}" -p "${port}" "echo \"${gateway} ota.ce\" >> /etc/hosts"
-  scp -P "${port}" ${options} "${device_dir}/client.pem" "root@${addr}:/var/sota/client.pem"
-  scp -P "${port}" ${options} "${device_dir}/pkey.pem" "root@${addr}:/var/sota/pkey.pem"
-}
-
 setup_credentials() {
   local api="${KUBE_API_URL}/${NAMESPACE}/services"
   local keyserver="${api}/tuf-keyserver/proxy"
@@ -170,7 +122,8 @@ new_server() {
   "data": {
   "server.key": "$(cat ${SERVER_DIR}/server.key | base64 -w 0)",
   "server.chain.pem": "$(cat ${SERVER_DIR}/server.chain.pem | base64 -w 0)",
-  "ca.crt": "$(cat ${SERVER_DIR}/devices/ca.crt | base64 -w 0)",
+  "ca.key": "$(cat ${DEVICES_DIR}/ca.key | base64 -w 0)",
+  "ca.crt": "$(cat ${DEVICES_DIR}/ca.crt | base64 -w 0)",
   "server_ca.pem": "$(cat ${SERVER_DIR}/server_ca.pem | base64 -w 0)"
   }
   }
